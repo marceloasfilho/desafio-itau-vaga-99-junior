875ab275bc6e5b319721fa1a6ce121dfbb58d0ff
marcelo.filho <mafilho4@stefanini.com>
2025-04-01 18:42:27 -0300

    - feature: commit inicial do projeto

---
+++ src/main/kotlin/com/marceloasfilho/transacoes/TransacoesApplication.kt
@@ -0,0 +1,11 @@
+package com.marceloasfilho.transacoes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TransacoesApplication
+
+fun main(args: Array<String>) {
+    runApplication<TransacoesApplication>(*args)
+}
---
+++ src/test/kotlin/com/marceloasfilho/transacoes/TransacoesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.marceloasfilho.transacoes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TransacoesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
