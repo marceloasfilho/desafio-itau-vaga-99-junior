3c72929ff3a86a1a689454f829c6c7d1a6b7d799
marcelo.filho <mafilho4@stefanini.com>
2025-04-02 09:22:32 -0300

    - log: adição de logs na camada de serviço

--- src/main/kotlin/com/marceloasfilho/transacoes/service/TransacaoService.kt
+++ src/main/kotlin/com/marceloasfilho/transacoes/service/TransacaoService.kt
@@ -5,22 +5,29 @@ import com.marceloasfilho.transacoes.model.Transacao
 import com.marceloasfilho.transacoes.model.dto.EstatisticaDTO
 import com.marceloasfilho.transacoes.model.dto.TransacaoDTO
 import com.marceloasfilho.transacoes.repository.TransacaoRepository
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.stereotype.Service
+
+private val logger = KotlinLogging.logger {}
 
 @Service
 class TransacaoService(private val transacaoRepository: TransacaoRepository) {
 
     fun salvaTransacao(transacao: TransacaoDTO): Transacao {
         val transacaoValida = transacao.toEntity()
+        logger.info { "Transação válida: $transacaoValida" }
         return this.transacaoRepository.salvar(transacaoValida)
     }
 
     fun deletaTodasTransacoes() {
         this.transacaoRepository.deletarTodasTransacoes()
+        logger.info { "Todas as transações foram deletadas com sucesso" }
     }
 
     fun obtemEstatisticasTransacoes(): EstatisticaDTO {
         val ultimasTransacoes = this.transacaoRepository.listarTransacoesUltimoMinuto()
+
+        logger.info { "Últimas transações ocorridas no último minuto: $ultimasTransacoes" }
 
         return when (ultimasTransacoes.size) {
             0 -> EstatisticaDTO()
